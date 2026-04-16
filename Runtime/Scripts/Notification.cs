8b416355ae0640aa6d563d592d5cb53bd4d6b681
Serbull <4weg4@mail.ru>
2026-04-16 18:45:09 +0700

    update notificator

--- Runtime/Scripts/Notification.cs
+++ Runtime/Scripts/Notification.cs
@@ -12,23 +12,18 @@ namespace Serbull.GameAssets
             Invoke(nameof(Hide), 2f);
         }
 
-        public void ShowGreen(string message)
+        public void Show(string message, Color color)
         {
             CancelInvoke();
             gameObject.SetActive(false);
             gameObject.SetActive(true);
             _text.text = message;
-            _text.color = Color.green;
+            _text.color = color;
         }
 
-        public void ShowRed(string message)
-        {
-            CancelInvoke();
-            gameObject.SetActive(false);
-            gameObject.SetActive(true);
-            _text.text = message;
-            _text.color = Color.red;
-        }
+        public void Show(string message) => Show(message, Color.white);
+        public void ShowGreen(string message) => Show(message, Color.green);
+        public void ShowRed(string message) => Show(message, Color.red);
 
         private void Hide()
         {
