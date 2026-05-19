b2c6f515b209301174f30b8f290b5bf12ca9b8d1
Serbull <4weg4@mail.ru>
2026-05-19 18:00:46 +0700

    remove bloom

--- Runtime/ThirdPartyAssets/Bloom/Scripts/RenderTextureExtensions.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace PostEffects
-{
-    static class Ext
-    {
-        public static RenderTextureFormat argbHalf = RenderTextureUtils.GetSupportedFormat(RenderTextureFormat.ARGBHalf);
-    }
-}
